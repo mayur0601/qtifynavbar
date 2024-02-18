f25ca129343280fe1556fd13cac3ab45595787c6
Mayur Shinde <shindemayur0601@gmail.com>
2024-02-18 19:21:01 +0530

    add show all and collapse

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -26,7 +26,7 @@ const Section = ({data,title,type}) => {
         <div className={styles.header}>
             <h3>{title}</h3>
             <h4 className={styles.toggleText} onClick={handleToggle} style={{color:'#34c94b'}}>
-                {!carouselToggle ? "Show All" : "Collapse All"}
+                {!carouselToggle ? "Show all" : "Collapse"}
             </h4>
         </div>
         {
